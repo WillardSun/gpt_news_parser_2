0c5985f1d58403946ee99be3fc4422d467803b7c
Willard <sunwillard@gmail.com>
2023-07-25 15:20:43 +0800

    changed auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 news_api_token = "4f178bdd222547bd9ad3e92d9ad44999"
 
-requesturl = "https://newsapi.org/v2/top-headlines?country=hk&apiKey=4f178bdd222547bd9ad3e92d9ad44999"
+requesturl = "https://newsapi.org/v2/top-headlines?country=hk&apiKey=" + news_api_token
 
 
     
