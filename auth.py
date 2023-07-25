f09e4f20ed3bf094792fad20bdef14e40f92c98e
Willard <sunwillard@gmail.com>
2023-07-25 15:19:33 +0800

    hello

--- auth.py
+++ auth.py
@@ -1,5 +1,3 @@
-token = "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"
-
 news_api_token = "4f178bdd222547bd9ad3e92d9ad44999"
 
 requesturl = "https://newsapi.org/v2/top-headlines?country=hk&apiKey=4f178bdd222547bd9ad3e92d9ad44999"
